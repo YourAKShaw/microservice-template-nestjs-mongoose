0af60a7cae44b8b4bf1a5ea5280f4d68a89baef1
YourAKShaw <hitme@yourakshaw.com>
2024-10-30 15:19:21 +0530

    feat(mongo): :card_file_box: incorporate MongooseModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import CustomLogger from '@src/common/logger';
 import { configValidationSchema } from '@src/config/config.validation';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       validationSchema: configValidationSchema,
-      isGlobal: true, // Make ConfigModule global
+      isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [
