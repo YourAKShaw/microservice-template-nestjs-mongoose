cd813c8b5af4afd1475e20037fec140193e37061
YourAKShaw <hitme@yourakshaw.com>
2024-10-30 15:10:19 +0530

    feat(config): :wrench: integrate ConfigModule and ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import CustomLogger from '@src/common/logger';
+import { configValidationSchema } from '@src/config/config.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValidationSchema,
+      isGlobal: true, // Make ConfigModule global
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
