58553169b079c81115721f1187f43234b453504b
YourAKShaw <hitme@yourakshaw.com>
2024-10-30 15:31:10 +0530

    feat(mongo): :zap: incorporated a basic user creation flow to demo mongo integration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from '@src/users/users.service';
+import { User } from '@src/users/schema/user.schema';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('age') age: number,
+  ): Promise<User> {
+    return this.userService.createUser(name, email, age);
+  }
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schema/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(name: string, email: string, age: number): Promise<User> {
+    const newUser = new this.userModel({ name, email, age });
+    return newUser.save();
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+}
