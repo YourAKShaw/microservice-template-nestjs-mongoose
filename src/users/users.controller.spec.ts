9c147775daeeeecadc188254a8d65286bdf1a88a
YourAKShaw <hitme@yourakshaw.com>
2025-04-22 17:39:07 +0530

    test(users): :white_check_mark: fix users.controller.spec.ts and users.service.spec.ts

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +9,16 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {}, // mock service
+        },
+        {
+          provide: JwtService,
+          useValue: {}, // mock JWT
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
